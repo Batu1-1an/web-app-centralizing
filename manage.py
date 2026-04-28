4fd0ca2a33e9c24e1d4b944c249b5ef5938fa1bf
batu_digitalexchange <batuaa70@gmail.com>
2026-04-28 11:19:30 +0300

    feat: add automatic .env loading via python-dotenv in manage.py

--- manage.py
+++ manage.py
@@ -2,11 +2,20 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from pathlib import Path
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "it_asset_manager.settings")
+
+    try:
+        from dotenv import load_dotenv
+    except ImportError:
+        pass
+    else:
+        load_dotenv(Path(__file__).resolve().parent / ".env")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
